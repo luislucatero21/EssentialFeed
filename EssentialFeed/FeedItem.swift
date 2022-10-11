9f02c3bb2359af8bd63a507239aa62854fdc31aa
Luis Angel Lucatero <luislucatero21@gmail.com>
2022-10-10 23:02:44 -0500

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by JibJab on 10/10/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
