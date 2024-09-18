d6fff9d0d67306d049842dd1309ef4d62ce5eb1a
Luis Lucatero <luislucatero21@gmail.com>
2024-09-17 21:32:54 -0600

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingView.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingView.swift
+//  EssentialFeed
+//
+//  Created by Luis Angel Lucatero Villanueva on 17/09/24.
+//
+
+protocol FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel)
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Luis Angel Lucatero Villanueva on 17/09/24.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
