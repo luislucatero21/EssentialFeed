1d1689e00823dd6629e32ff80daf41fa568664ea
Luis Lucatero <luislucatero21@gmail.com>
2024-09-27 20:55:42 -0600

    Create test helper to find missing localized strings

---
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/Helpers/FeedViewControllerTests+Localization.swift
@@ -0,0 +1,22 @@
+//
+//  FeedViewControllerTests+Localization.swift
+//  EssentialFeed
+//
+//  Created by Luis Angel Lucatero Villanueva on 27/09/24.
+//
+
+import Foundation
+import XCTest
+import EssentialFeediOS
+
+extension FeedViewControllerTests {
+    func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: FeedViewController.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(table)", file: file, line: line)
+        }
+        return value
+    }
+}
